dd87beb2eb10acfc0d6f510da6ee0d196be3d3ff
Khalid T <khalid.tissire@gmail.com>
2020-05-04 03:29:04 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,13 @@
 class BirdsController < ApplicationController
+
   def index
     birds = Bird.all
-    render json: birds, except: [:created_at, :updated_at]
+    render json: birds, only: [:id, :name, :species]
   end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    render json: bird
+  end
+
 end
